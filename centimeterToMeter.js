c03ac1c5df715be96b7b734c26f0929bed0e3702
Md Shamsul Huda Chowdhury <plabonchowdhury007@gmail.com>
2023-08-06 13:39:05 +0600

    first commit

---
+++ centimeterToMeter.js
@@ -0,0 +1,9 @@
+// 1 centimeter = 0.01 meter
+function centimeterToMeter(valueOfCentimeter) {
+    const centimeterValue = valueOfCentimeter * 0.01
+    return centimeterValue;
+    
+}
+
+const result = centimeterToMeter(250);
+console.log("meter value is :", result);
