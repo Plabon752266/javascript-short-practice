8bcc659d73d89f1ab971f45396398a4a5e10673c
Md Shamsul Huda Chowdhury <plabonchowdhury007@gmail.com>
2023-08-06 13:19:09 +0600

    first commit

---
+++ feetToInch.js
@@ -0,0 +1,12 @@
+// convert feet to inch
+function feetToInch(feetValue) {
+    // 12 inch = 1feet
+    const inchValue = feetValue * 12;
+    return inchValue;
+    
+    
+}
+
+
+const result = feetToInch(12);
+console.log('inch value hocche :', result);
