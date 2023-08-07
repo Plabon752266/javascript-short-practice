dbabab05d16c4c1948ac0974f9023cfdfafffa02
Md Shamsul Huda Chowdhury <plabonchowdhury007@gmail.com>
2023-08-07 18:26:50 +0600

    first commit

---
+++ isInteger.js
@@ -0,0 +1,23 @@
+// তোমাকে এমন একটা ফাংশন বানাতে হবে যেটা একটা সংখ্যার ইনপুট নিবে। ফাংশন টির নাম হবে isInteger().আউটপুট হিসেবে একটি boolean রিটার্ন করবে। 
+// সংখ্যাটি integer হলে true রিটার্ন করবে। 
+// অন্যথায় false রিটার্ন করবে 
+
+
+function isInteger(num) {
+    if (typeof num !== "number") {
+        return "pls provide me a number"
+        
+    }
+    else{
+        if (num % 1 === 0) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+}
+
+const outputGivenNumber = isInteger(825);
+console.log(outputGivenNumber);
